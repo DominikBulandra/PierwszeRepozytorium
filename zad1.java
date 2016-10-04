aa3cc6900ca2f4f53642e16b688bc256b98d8681
Dominik <d-o-m-i-n@tlen.pl>
2016-10-04 14:15:34 +0200

    zadanie 1 a

--- zad1.java
+++ zad1.java
@@ -8,8 +8,15 @@ public static void main(String[] args) {
 	int min=klawiatura.nextInt();
 	System.out.println("Podaj maximum ");
 	int max=klawiatura.nextInt();
-	Random rand=new Random();
+	if(max<min)
+	{
+		System.out.println("Zle parametry max < min ");
+	}
+	else
+	{
+Random rand=new Random();
 	int a = rand.nextInt(max - min + 1) + min;
+
 	System.out.println("wylosowano liczbe: "+ a);
 	 for(int i=1; i<1000; i++){
 		if(i%a==0){
@@ -19,6 +26,9 @@ public static void main(String[] args) {
 
               
          }
+
+	}	
+	
         
 }
 }
